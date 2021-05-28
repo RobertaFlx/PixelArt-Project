273e7e78c390dfa46e65c415fc457f49d6705080
Roberta Felix <berttafellix1@gmail.com>
2021-05-28 17:04:44 -0300

    Inicial commit

---
+++ src/test/java/pixelartcom/pixel/PixelApplicationTests.java
@@ -0,0 +1,13 @@
+package pixelartcom.pixel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PixelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
